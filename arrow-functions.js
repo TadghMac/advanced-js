66cafd5976ca8cc01548b33413a5f835922d03fd
Tadgh Mc Carthy <tadghmccarthy618@gmail.com>
2024-01-30 16:14:35 +0000

    arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,25 @@
  */
 
 // Vanilla JavaScript Function
-
+function addTwoNumbers (a,b) {
+    return a + b;
+}
+let sum = addTwoNumbers(3,5);
+console.log(sum);
 // Arrow Function With Parameters
+const addTwoNumbers = (a,b) => {
+    return a + b;
+}
+let sum = addTwoNumbers(3,5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers = (a,b) => a + b
 // Implicit Returns
+const saySome = message => console.log(message);
+saySome('Hello there!!')
+
+const saySome = () => console.log('hello');
+saySome();
 
 // Returning Multiple Lines
